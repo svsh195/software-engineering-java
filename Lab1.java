c066a6ab618c01bbc9c4d8b82cfa57671de2ee3f
svsh195 <78136785+svsh195@users.noreply.github.com>
2021-02-25 10:10:53 +0700

    Create Lab1.java

---
+++ Lab1.java
@@ -0,0 +1,16 @@
+public class Program{ 
+      
+    public static void main (String args[]){
+        var a = 0;
+        var x = 12 * Math.pow(a, 2) - 16;
+        var y = 7 * Math.pow(x, 2) - 3 * x + 6;
+
+        var b = 0;
+        var perimeter = 2 * (a + b);
+        var square = a * b;
+
+        var celsius = 0;
+        var fahrenheit = celsius * 1.8 + 32;
+        var kelvin = celsius + 273.15;
+    }
+}
